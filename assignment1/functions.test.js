51d7dd1298d0c0f4ebf9e92987426054e9aa586e
jakxc <jack.x.chen@outlook.com>
2023-08-16 17:23:03 +1000

    Updated unit tests
    - Restructured console table display

--- assignment1/functions.test.js
+++ assignment1/functions.test.js
@@ -1,68 +1,42 @@
-const fs = require('fs');
 const functions = require('./functions');
-const data = fs.readFileSync("NEOWISE_Dataset.json", "utf8");
+const tempData = [
+{ designation: "123", orbit_class: "Apollo", period_yr: 4.06, pha: true},
+{ designation: "345", orbit_class: "Apollo", period_yr: 4.06, pha: true},
+{ designation: "567", orbit_class: "Aten", period_yr: 4.06, pha: true},
+{ designation: "789", orbit_class: "Amor", period_yr: 4.06, pha: true},
+{ designation: "012", orbit_class: "Amor", period_yr: 4.06, pha: true},]
 
-const parseData = functions.parseData;
 const getNeoByIndex = functions.getNeoByIndex;
 const getNeoByDesignation = functions.getNeoByDesignation;
 const getNeoByClass = functions.getNeoByClass;
 
 // Test with an input of 0 for getNeoByIndex
-test ('Input of data and 0, output of first element in data array', () => {
-    expect(getNeoByIndex(parseData(data), 0)).toEqual({
-                                                        designation: "419880 (2011 AH37)", 
-                                                        discovery_date: "2011-01-07T00:00:00.000",
-                                                        h_mag: 19.7, 
-                                                        i_deg: 9.65, 
-                                                        moid_au: 0.035,
-                                                        orbit_class: "Apollo", 
-                                                        period_yr: 4.06,
-                                                        pha: true,
-                                                        q_au_1: 0.84,
-                                                        q_au_2: 4.26
-                                                    });
+test ('Input of data and index, output should return element at that index in data', () => {
+    expect(getNeoByIndex(tempData, 0)).toEqual({ designation: "123", orbit_class: "Apollo", period_yr: 4.06, pha: true});
 });
 
-test ('Input of data and "398188 (2010 LE15)", output of element with designation matching "398188 (2010 LE15)"',
-    () => {
-        expect(getNeoByDesignation(parseData(data), "398188 (2010 LE15)")).toEqual([
-            {
-              designation: '398188 (2010 LE15)',
-              discovery_date: '2010-06-03T00:00:00.000',
-              h_mag: 19.5,
-              moid_au: 0.024,
-              q_au_1: 0.63,
-              q_au_2: 1.1,
-              period_yr: 0.8,
-              i_deg: 13.25,
-              pha: true,
-              orbit_class: 'Aten'
-            }
-          ])
-    }
-)
+// Test with an input of 0 for getNeoByIndex
+test ('Input of data and invalid index, output should return null', () => {
+  expect(getNeoByIndex(tempData, -1)).toBeNull();
+});
 
-test ('Input of data and Parabolic Comet, output of element with orbit_class matching Parabolic Comet',
-    () => {
-        expect(getNeoByClass(parseData(data), "Parabolic Comet")).toEqual([
-              {
-                designation: 'C/2010 J4 (WISE)',
-                discovery_date: '2010-05-12T00:00:00.000',
-                moid_au: 0.307,
-                q_au_1: 1.09,
-                i_deg: 162.3,
-                pha: null,
-                orbit_class: 'Parabolic Comet'
-              },
-              {
-                designation: 'C/2010 E3 (WISE)',
-                discovery_date: '2010-03-05T00:00:00.000',
-                moid_au: 1.546,
-                q_au_1: 2.27,
-                i_deg: 96.48,
-                pha: null,
-                orbit_class: 'Parabolic Comet'
-              }
-            ])
-    }
-)
+test ('Input of data and designation value, output of element with matching designation', () => {
+  expect(getNeoByDesignation(tempData, "567")).toEqual([
+    { designation: "567", orbit_class: "Aten", period_yr: 4.06, pha: true}
+  ])
+})
+
+test ('Input of data and invalid designation value, output should be null', () => {
+  expect(getNeoByDesignation(tempData, "abc")).toBeNull();
+})
+
+test ('Input of data and orbit_class, output of element with matching orbit_class in data', () => {
+  expect(getNeoByClass(tempData, "Amor")).toEqual([
+    { designation: "789", orbit_class: "Amor", period_yr: 4.06, pha: true},
+    { designation: "012", orbit_class: "Amor", period_yr: 4.06, pha: true}
+  ])
+})
+
+test ('Input of data and orbit_class, output should have length equal to number of elements with matching class', () => {
+  expect(getNeoByClass(tempData, "Apollo")).toHaveLength(2);
+})
--- assignment1/script.js
+++ assignment1/script.js
@@ -45,31 +45,109 @@ function main() {
         phaArr.push(...neoDataObj.getNeoByPha(true));
       }
     }
+    
+    const phaNeoData = new NeoData(phaArr);
+    console.table(phaNeoData.getMaxOrbitValue('h_mag'));
 
-    const phaDataObj = new NeoData(phaArr);
-    // Determine maximum Minimum Orbit Intersection Distance (MOID) of all PHA is dataset
-    const maxMoidPhaObj = new NeoData(phaDataObj.getMaxOrbitValue("moid_au"));
-    // maxMoidPhaObj.displayAllNeoInfo();
+    // Determine minimum/maximum/average q_au_1 values of all PHA is dataset
+    const minPhaQ1 = phaNeoData.getMinOrbitValue("q_au_1") ? phaNeoData.getMinOrbitValue("q_au_1")[0]["q_au_1"] : null;
+    const maxPhaQ1 = phaNeoData.getMaxOrbitValue("q_au_1") ? phaNeoData.getMaxOrbitValue("q_au_1")[0]["q_au_1"] : null;
+    const avgPhaQ1 = +phaNeoData.getAverageOrbitValue("q_au_1").toFixed(3);
 
-    // Determine minimum observed magnitude or brightness (h_mag) of all PHA is dataset
-    const minMagPhaObj = new NeoData(phaDataObj.getMaxOrbitValue("h_mag"));
-    // minMagPhaObj.displayAllNeoInfo();
+    // Determine minimum/maximum/average q_au_2 values of all PHA is dataset
+    const minPhaQ2 = phaNeoData.getMinOrbitValue("q_au_2") ? phaNeoData.getMinOrbitValue("q_au_2")[0]["q_au_2"] : null;
+    const maxPhaQ2 = phaNeoData.getMaxOrbitValue("q_au_2") ? phaNeoData.getMaxOrbitValue("q_au_1")[0]["q_au_2"] : null;
+    const avgPhaQ2 = +phaNeoData.getAverageOrbitValue("q_au_2").toFixed(3);
+    
+    // Determine minimum/maximum/average Minimum Orbit Intersection Distance (MOID) of all PHA is dataset
+    const minPhaMoid = phaNeoData.getMinOrbitValue("moid_au") ? phaNeoData.getMinOrbitValue("moid_au")[0]["moid_au"] : null;
+    const maxPhaMoid = phaNeoData.getMaxOrbitValue("moid_au") ? phaNeoData.getMaxOrbitValue("moid_au")[0]["moid_au"] : null;
+    const avgPhaMoid = +phaNeoData.getAverageOrbitValue("moid_au").toFixed(3);
 
-    // Determine minimum and maximum perihelion distance (q_au_1) of all Amors in dataset
-    const amorArr = sortedDataByClass["Amor"];
-    const amorDataObj = new NeoData(amorArr);
-    const minQ1AmorArr = new NeoData(amorDataObj.getMinOrbitValue("q_au_1"));
-    const maxQ1AmorArr = new NeoData(amorDataObj.getMaxOrbitValue("q_au_1"));
+    // Determine minimum/maximum/average observed magnitude (or brightness) (h_mag) of all PHA is dataset
+    const minPhaMag = phaNeoData.getMinOrbitValue("h_mag") ? phaNeoData.getMinOrbitValue("h_mag")[0]["h_mag"] : null;
+    const maxPhaMag = phaNeoData.getMaxOrbitValue("h_mag") ? phaNeoData.getMaxOrbitValue("h_mag")[0]["h_mag"] : null;
+    const avgPhaMag = +phaNeoData.getAverageOrbitValue("h_mag").toFixed(3);
+
+    const phaDataObj = {} 
+    phaDataObj["min q1"] = minPhaQ1;
+    phaDataObj["max q1"] = maxPhaQ1;
+    phaDataObj["avg q1"] = avgPhaQ1;
+    phaDataObj["min q2"] = minPhaQ2;
+    phaDataObj["max q2"] = maxPhaQ2;
+    phaDataObj["avg q2"] = avgPhaQ2;
+    phaDataObj["min moid"] = minPhaMoid;
+    phaDataObj["max moid"] = maxPhaMoid;
+    phaDataObj["avg moid"] = avgPhaMoid;
+    phaDataObj["min mag"] = minPhaMag;
+    phaDataObj["max mag"] = maxPhaMag;
+    phaDataObj["avg mag"] = avgPhaMag;
+
+    console.table(phaDataObj);
+    
+    // Determine minimum/maximum/average of all orbit values and of all NEO classes in dataset
+    const newDataObj = {};
+    for (const key in sortedDataByClass) {
+      const tempDataObj = {};
+      const neoClassArr = sortedDataByClass[key];
+      const neoDataObj = new NeoData(neoClassArr);
+     
+      const minQ1 = neoDataObj.getMinOrbitValue("q_au_1") ? neoDataObj.getMinOrbitValue("q_au_1")[0]["q_au_1"] : null;
+      const minQ2 = neoDataObj.getMinOrbitValue("q_au_2") ? neoDataObj.getMinOrbitValue("q_au_2")[0]["q_au_2"] : null;
+      const minMoid = neoDataObj.getMinOrbitValue("moid_au") ? neoDataObj.getMinOrbitValue("moid_au")[0]["moid_au"] : null;
+      const minMag = neoDataObj.getMinOrbitValue("h_mag") ? neoDataObj.getMinOrbitValue("h_mag")[0]["h_mag"] : null;
+      const minPeriodYr = neoDataObj.getMinOrbitValue("period_yr") ? neoDataObj.getMinOrbitValue("period_yr")[0]["period_yr"] : null;
+
+      const maxQ1 = neoDataObj.getMaxOrbitValue("q_au_1") ? neoDataObj.getMaxOrbitValue("q_au_1")[0]["q_au_1"] : null;
+      const maxQ2 = neoDataObj.getMaxOrbitValue("q_au_2") ? neoDataObj.getMaxOrbitValue("q_au_2")[0]["q_au_2"] : null;
+      const maxMoid = neoDataObj.getMaxOrbitValue("moid_au") ? neoDataObj.getMaxOrbitValue("moid_au")[0]["moid_au"] : null;
+      const maxMag = neoDataObj.getMaxOrbitValue("h_mag") ? neoDataObj.getMaxOrbitValue("h_mag")[0]["h_mag"] : null;
+      const maxPeriodYr = neoDataObj.getMaxOrbitValue("period_yr") ? neoDataObj.getMaxOrbitValue("period_yr")[0]["period_yr"] : null;
+
+      const avgQ1 = neoDataObj.getAverageOrbitValue("q_au_1") ? +neoDataObj.getAverageOrbitValue("q_au_1").toFixed(3) : null;
+      const avgQ2 = neoDataObj.getAverageOrbitValue("q_au_2") ? +neoDataObj.getAverageOrbitValue("q_au_2").toFixed(3) : null;
+      const avgMoid = neoDataObj.getAverageOrbitValue("moid_au") ? +neoDataObj.getAverageOrbitValue("moid_au").toFixed(3) : null;
+      const avgMag = neoDataObj.getAverageOrbitValue("h_mag") ? +neoDataObj.getAverageOrbitValue("h_mag").toFixed(3) : null;
+      const avgPeriodYr = neoDataObj.getAverageOrbitValue("period_yr") ? +neoDataObj.getAverageOrbitValue("period_yr").toFixed(3) : null;
+      
+      // q_au_1 values
+      tempDataObj["min q1"] = minQ1;
+      tempDataObj["max q1"] = maxQ1;
+      tempDataObj["avg q1"] = avgQ1;
+
+      // q_au_2 values
+      tempDataObj["min q2"] = minQ2;
+      tempDataObj["max q2"] = maxQ2;
+      tempDataObj["avg q2"] = avgQ2;
+
+      // moid values
+      tempDataObj["min moid"] = minMoid;
+      tempDataObj["max moid"] = maxMoid;
+      tempDataObj["avg moid"] = avgMoid;
+
+      // period_yr values
+      tempDataObj["min period"] = minPeriodYr;
+      tempDataObj["max period"] = maxPeriodYr;
+      tempDataObj["avg period"] = avgPeriodYr;
+
+      newDataObj[key] = tempDataObj;    
+    }
+
+    console.table(newDataObj);
+
+    // // Determine minimum and maximum perihelion distance (q_au_1) of all Amors in dataset
+    // const amorArr = sortedDataByClass["Amor"];
+    // const amorDataObj = new NeoData(amorArr);
+    // const minQ1AmorArr = new NeoData(amorDataObj.getMinOrbitValue("q_au_1"));
+    // const maxQ1AmorArr = new NeoData(amorDataObj.getMaxOrbitValue("q_au_1"));
     // minQ1AmorArr.displayAllNeoInfo(); 
     // maxQ1AmorArr.displayAllNeoInfo(); 
 
-    // Determine the maximum perihelion distance (q_au_1) of all Apollos in dataset
+    // // Determine the maximum perihelion distance (q_au_1) of all Apollos in dataset
     // const apolloArr = sortedDataByClass["Apollo"];
     // const apolloDataObj = new NeoData(apolloArr);
     // const maxQ1ApolloArr = new NeoData(apolloDataObj.getMaxOrbitValue("q_au_1"));
     // maxQ1ApolloArr.displayAllNeoInfo();
-
-    console.log(functions.getNeoByClass(parseData(data), "Parabolic Comet"))
 }
 
 main();
